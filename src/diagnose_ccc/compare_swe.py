dd4311f0a6c054e22bc77b8196477a34fd3ec8c0
Huziy Oleksandr <guziy.sasha@gmail.com>
2011-09-11 20:43:27 -0400

    added selection by mask

--- src/diagnose_ccc/compare_swe.py
+++ src/diagnose_ccc/compare_swe.py
@@ -3,6 +3,7 @@
 __author__="huziy"
 __date__ ="$Aug 5, 2011 10:44:05 AM$"
 
+from matplotlib.ticker import LinearLocator
 from data.swe_ross_brown import SweHolder
 from util.geo.GeoPoint import GeoPoint
 from netCDF4 import Dataset
@@ -26,6 +27,8 @@ import matplotlib as mpl
 
 import util.plot_utils as plot_utils
 
+import matplotlib as mpl
+import shape.basin_boundaries as bb
 
 def getBasinMask(basinName = 'RDO'):
     '''
@@ -38,11 +41,66 @@ def getBasinMask(basinName = 'RDO'):
     return mask
 
 
+def get_domain_mask(path = 'data/infocell/amno180x172_basins.nc'):
+    ds = Dataset(path)
+    result = None
+    for v in ds.variables.values():
+        if result == None:
+            result = v[:]
+        else:
+            result += v[:]
+    ds.close()
+    return result
+
+
 def getSelectedBasinNames():
     return ['BAL', 'RDO', 'LGR']
 
 
-def getCCCDataForMask(mask = None, path_to_ccc = 'data/ccc_data/aex/aex_p1sno',
+
+def getTemporalMeanCCCDataForMask(mask = None, path_to_ccc = 'data/ccc_data/aex/aex_p1sno',
+                    startDate = None, endDate = None, months = []):
+
+    '''
+    months - is a list of month indices (i.e. 1,2,3,4 .. 12), over which
+    the mean is calculated
+    returns
+    '''
+
+    dt = timedelta(hours = 6)
+    dates = []
+    result = {}
+    for fName in os.listdir(path_to_ccc):
+        fPath = os.path.join( path_to_ccc, fName )
+        cccObj = champ_ccc(fPath)
+        theDate = datetime.strptime( fName.split('_')[-1][:-4], '%Y%m')
+
+        if endDate != None and startDate != None:
+            if endDate < theDate or theDate < startDate:
+                continue
+
+        dataStore = cccObj.charge_champs()
+        for field in dataStore:
+            if startDate != None and endDate != None:
+                if startDate > theDate:
+                    continue
+                if endDate < theDate:
+                    break
+
+            if theDate.month not in months:
+                continue
+
+            fieldData = field['field']
+            dates.append(theDate)
+            result[theDate] = fieldData[mask == 1]
+            theDate += dt
+
+    x = np.array(result.values())
+    return np.mean(x, axis = 0)
+
+
+
+def getSpatialMeanCCCDataForMask(mask = None, path_to_ccc = 'data/ccc_data/aex/aex_p1sno',
                     startDate = None, endDate = None):
 
     '''
@@ -72,7 +130,7 @@ def getCCCDataForMask(mask = None, path_to_ccc = 'data/ccc_data/aex/aex_p1sno',
             
             fieldData = field['field']
             dates.append(theDate)
-            result[theDate] = np.mean(fieldData[mask == 1])
+            result[theDate] = np.mean(fieldData[mask == 1]) 
             theDate += dt
     dates.sort()
     return dates, [result[d] for d in dates]
@@ -84,7 +142,7 @@ def test():
     end = datetime(1986, 12, 31,00)
     basinName = 'RDO'
     mask = getBasinMask(basinName)
-    print getCCCDataForMask(mask, startDate = start, endDate = end)
+    print getSpatialMeanCCCDataForMask(mask, startDate = start, endDate = end)
 
 
 
@@ -100,23 +158,56 @@ def toStampYear(theDate, stamp_year = 2000):
         print theDate
 
 
-def compare_means(basinName = 'RDO'):
-    lons = polar_stereographic.lons
-    lats = polar_stereographic.lats
+def compare_means(basinName = 'RDO',
+                  start = datetime(1980,01,01,00),
+                  end = datetime(1996, 12, 31,00)):
 
     mask = getBasinMask(basinName)
+    compare_daily_normals_mean_over_mask(mask, start = start, end = end, label = basinName)
 
+
+
+
+def compare_spatial_means_over_mask(mask = None, label = ''):
+    '''
+    timeseries not changed, averaging over the points where mask == 1
+    '''
     start = datetime(1980,01,01,00)
     end = datetime(1996, 12, 31,00)
+
+    lons = polar_stereographic.lons
+    lats = polar_stereographic.lats
 
     lons_selected = lons[ mask == 1 ]
     lats_selected = lats[ mask == 1 ]
     points = [GeoPoint(longitude = lon, latitude = lat) for lon, lat in zip(lons_selected, lats_selected)]
 
     sweObs = SweHolder()
-    obsData = sweObs.getMeanDataFromNetCDFforPoints(points, startDate = start, endDate = end)
+    obsData = sweObs.getSpatialMeanDataFromNetCDFforPoints(points, startDate = start, endDate = end)
+    modelData = getSpatialMeanCCCDataForMask(mask, startDate = start, endDate = end)
+
+    plt.figure()
+    plt.title('SWE' + ' ' + label)
+    plt.plot(obsData[0], obsData[1], label = 'Obs.', color = 'red', lw = 3)
+    plt.plot(modelData[0], modelData[1], label = 'Model', color = 'blue', lw = 3)
+    plt.ylabel('mm')
+    plt.legend()
+    plt.savefig('swe_compare_{0}.pdf'.format(label), bbox_inches = 'tight')
+
+
+def compare_daily_normals_mean_over_mask(mask = None, start = None, end = None, label = ''):
+    lons = polar_stereographic.lons
+    lats = polar_stereographic.lats
+
+    lons_selected = lons[ mask == 1 ]
+    lats_selected = lats[ mask == 1 ]
+
+    points = [GeoPoint(longitude = lon, latitude = lat) for lon, lat in zip(lons_selected, lats_selected)]
+
+    sweObs = SweHolder()
+    obsData = sweObs.getSpatialMeanDataFromNetCDFforPoints(points, startDate = start, endDate = end)
     print 'finished reading observations'
-    modelData = getCCCDataForMask(mask, startDate = start, endDate = end)
+    modelData = getSpatialMeanCCCDataForMask(mask, startDate = start, endDate = end)
     print 'finished reading model data'
     print 'finished reading input mean timeseries'
 
@@ -157,8 +248,8 @@ def compare_means(basinName = 'RDO'):
     print 'Calculated mean for day of year and over a basin points'
 
 
-    plt.figure()
-    plt.title(basinName + ': SWE')
+    plt.figure(figsize = (8, 6), dpi = 80)
+    plt.title('SWE {0}'.format(label))
     plt.plot(obsDates, obsMeanValues, label = 'Obs.', color = 'red', lw = 3)
     plt.plot(modelDates, modelMeanValues, label = 'Model', color = 'blue', lw = 3)
     plt.ylabel('mm')
@@ -169,34 +260,12 @@ def compare_means(basinName = 'RDO'):
         mpl.dates.MonthLocator(bymonth = range(2,13,2))
     )
     plt.legend()
-    plt.savefig(basinName + '_swe.pdf', bbox_inches = 'tight')
-
+    plt.savefig(label + '_swe.pdf', bbox_inches = 'tight')
 
 
 def compare(basinName = 'RDO'):
-    lons = polar_stereographic.lons
-    lats = polar_stereographic.lats
-
     mask = getBasinMask(basinName)
-
-    start = datetime(1985,01,01,00)
-    end = datetime(1986, 12, 31,00)
-
-    lons_selected = lons[ mask == 1 ]
-    lats_selected = lats[ mask == 1 ]
-    points = [GeoPoint(longitude = lon, latitude = lat) for lon, lat in zip(lons_selected, lats_selected)]
-
-    sweObs = SweHolder()
-    obsData = sweObs.getMeanDataFromNetCDFforPoints(points, startDate = start, endDate = end)
-    modelData = getCCCDataForMask(mask, startDate = start, endDate = end)
-
-    plt.figure()
-    plt.title('SWE')
-    plt.plot(obsData[0], obsData[1], label = 'Obs.', color = 'red', lw = 3)
-    plt.plot(modelData[0], modelData[1], label = 'Model', color = 'blue', lw = 3)
-    plt.ylabel('mm')
-    plt.legend()
-    plt.show()
+    compare_spatial_means_over_mask(mask = mask)
 
 
 
@@ -208,11 +277,62 @@ def getBasinNames():
     return names
 
 
-if __name__ == "__main__":
-    application_properties.set_current_directory()
-    #test()
-    #compare_means('BEL')
+
+def compare_swe_2d():
+    '''
+    Compare seasonal mean
+    '''
+    start = datetime(1980,01,01,00)
+    end = datetime(1996, 12, 31,00)
+    months = [2,3,4]
+    #calculate mean for ccc data accounting for the mask
+    domain_mask = get_domain_mask()
+    cccData = getTemporalMeanCCCDataForMask(domain_mask,
+                                            startDate = start, endDate = end, months = months)
+    lon_selected = polar_stereographic.lons[domain_mask == 1]
+    lat_selected = polar_stereographic.lats[domain_mask == 1]
+
+    geopointList = []
+    for lon, lat in zip(lon_selected, lat_selected):
+        geopointList.append(GeoPoint(longitude = lon, latitude = lat))
+
+    print 'Selecting obs data'
+    sweObs = SweHolder()
+    obsData = sweObs.getTemporalMeanDataFromNetCDFforPoints(geopointList, startDate = start,
+                                                            endDate = end, months = months)
+
     
+
+    to_plot = np.ma.masked_all(polar_stereographic.lons.shape)
+    condition = domain_mask == 1
+    to_plot[condition] = (cccData - obsData) / obsData * 100
+    xs = polar_stereographic.xs
+    ys = polar_stereographic.ys
+    basemap = polar_stereographic.basemap
+
+    plot_utils.apply_plot_params()
+    basemap.pcolormesh(xs, ys, to_plot, cmap = mpl.cm.get_cmap('jet', 7))
+    basemap.drawcoastlines()
+    plt.colorbar(ticks = LinearLocator(numticks = 8), format = '%.1f')
+    plt.title('Snow Water Equivalent (%) \n $(S_{\\rm CRCM4} - S_{\\rm obs.})/S_{\\rm obs.}\\cdot100\%$\n')
+
+    #zoom to domain
+    selected_x = xs[~to_plot.mask]
+    selected_y = ys[~to_plot.mask]
+    marginx = abs(np.min(selected_x) * 5.0e-2)
+    marginy = abs(np.min(selected_y) * 5.0e-2)
+
+    plt.xlim(np.min(selected_x) - marginx, np.max(selected_x) + marginx)
+    plt.ylim(np.min(selected_y) - marginy, np.max(selected_y) + marginy)
+
+    bb.plot_basin_boundaries_from_shape(basemap, plotter = plt, linewidth = 2, edge_color = 'k')
+    plt.savefig('swe_djf_validation.pdf', bbox_inches = 'tight')
+
+
+    pass
+
+
+def compare_averages_over_basins():
     print getBasinNames()
     plot_utils.apply_plot_params(width_pt = 400, font_size = 15)
 
@@ -220,6 +340,14 @@ if __name__ == "__main__":
     for basinName in basinNames:
         compare_means(basinName = basinName)
         print basinName
+
+
+if __name__ == "__main__":
+    application_properties.set_current_directory()
+    #test()
+    #compare_means('BEL')
+    compare_swe_2d()
+    
      
     
     print "Hello World"
