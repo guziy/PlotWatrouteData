fix time units in swe file

src/data/swe_ross_brown.py
@@ -1,11 +1,10 @@
 import codecs
 import re
-from mpl_toolkits.basemap import Basemap, maskoceans
+from mpl_toolkits.basemap import Basemap
 import matplotlib as mpl
-import netCDF4
 
-__author__="huziy"
-__date__ ="$Aug 4, 2011 5:06:41 PM$"
+__author__ = "huziy"
+__date__ = "$Aug 4, 2011 5:06:41 PM$"
 
 from util.geo.GeoPoint import GeoPoint
 import os
@@ -20,20 +19,21 @@ from netCDF4 import date2num, num2date
 from matplotlib.ticker import LogLocator
 from matplotlib import colors
 
+
 class SweHolder:
     """
     point (1,1) - is a lower left point of the grid
     """
-    def __init__(self, path = 'data/swe_ross_brown/derksen'):
+
+    def __init__(self, path='data/swe_ross_brown/derksen'):
         self.root_path = path
         self.step = 0.25
-        self.lowerLeft = GeoPoint(longitude = -160.0, latitude = 0.0)
+        self.lowerLeft = GeoPoint(longitude=-160.0, latitude=0.0)
         self.nEastWest = 480
         self.nNorthSouth = 360
         self.file_name_format = '%Y%m%d%H'
 
         self.all_dates = []
-
 
         self.pathToNetCDF = 'data/swe_ross_brown/swe.nc'
         self._initDates()
@@ -47,7 +47,6 @@ class SweHolder:
         lon0 = self.lowerLeft.longitude
         lat0 = self.lowerLeft.latitude
 
-
         lons = np.arange(lon0, lon0 + self.nEastWest * self.step, self.step)
         lats = np.arange(lat0, lat0 + self.nNorthSouth * self.step, self.step)
 
@@ -55,7 +54,7 @@ class SweHolder:
         return lons, lats
 
 
-    def get_mean_for_months(self, months = None):
+    def get_mean_for_months(self, months=None):
         if not months: months = []
         pass
 
@@ -63,9 +62,8 @@ class SweHolder:
     def _initDates(self):
         self.all_dates = []
         for fName in os.listdir(self.root_path):
-            self.all_dates.append( datetime.strptime( fName.split(".")[0], self.file_name_format))
+            self.all_dates.append(datetime.strptime(fName.split(".")[0], self.file_name_format))
         self.all_dates.sort()
-
 
     def getStartDate(self):
         return min(self.all_dates)
@@ -77,8 +75,10 @@ class SweHolder:
         """
         months - months of year over which the average is taken
         """
-        if not months: months = []
-        if not geopointList: geopointList = []
+        if not months:
+            months = []
+        if not geopointList:
+            geopointList = []
         i_indices = []
         j_indices = []
         for point in geopointList:
@@ -92,7 +92,7 @@ class SweHolder:
         ds = Dataset(self.pathToNetCDF)
 
         times = ds.variables['time']
-        times = num2date(times[:], units = times.units)
+        times = num2date(times[:], units=times.units)
 
         data = ds.variables['SWE']
         query = (times >= startDate) & (times <= endDate)
@@ -100,17 +100,17 @@ class SweHolder:
             if not query[i]: continue
             if times[i].month not in months:
                 query[i] = False
-        return np.mean(data[query, i_indices, j_indices], axis = 0)
+        return np.mean(data[query, i_indices, j_indices], axis=0)
 
-    def _get_na_0_25_mask(self, path = "data/swe_ross_brown/na_lmask_edit.txt"):
+    def _get_na_0_25_mask(self, path="data/swe_ross_brown/na_lmask_edit.txt"):
         """
         reads in 0 and 1 mask of na, source : Ross Brown
         """
         f = open(path)
         lines = f.readlines()
         f.close()
-        mask = map(lambda x: map(int, re.findall("\d", x)), lines )
-        return np.array(mask).transpose()[:,::-1]
+        mask = map(lambda x: map(int, re.findall("\d", x)), lines)
+        return np.array(mask).transpose()[:, ::-1]
 
 
     def getSpatialIntegralFromNetcdfForPoints(self, geopointList=None, startDate=None, endDate=None, months=None):
@@ -132,19 +132,19 @@ class SweHolder:
         ds = Dataset(self.pathToNetCDF)
 
         times = ds.variables['time']
-        times = num2date(times[:], units = times.units)
+        times = num2date(times[:], units=times.units)
 
         data = ds.variables['SWE']
         if not len(months):
             query = (times >= startDate) & (times <= endDate)
-            return times[query][:], np.sum(data[query, i_indices, j_indices], axis = 1)
+            return times[query][:], np.sum(data[query, i_indices, j_indices], axis=1)
         else:
             query = (times >= startDate) & (times <= endDate)
             for i in xrange(len(times)):
                 if not query[i]: continue
                 if times[i].month not in months:
                     query[i] = False
-            return times[query][:], np.sum(data[query, i_indices, j_indices], axis = 1)
+            return times[query][:], np.sum(data[query, i_indices, j_indices], axis=1)
 
 
     def getSpatialMeanDataFromNetCDFforPoints(self, geopointList=None, startDate=None, endDate=None, months=None):
@@ -166,20 +166,19 @@ class SweHolder:
         ds = Dataset(self.pathToNetCDF)
 
         times = ds.variables['time']
-        times = num2date(times[:], units = times.units)
+        times = num2date(times[:], units=times.units)
 
         data = ds.variables['SWE']
         if not len(months):
             query = (times >= startDate) & (times <= endDate)
-            return times[query][:], np.mean(data[query, i_indices, j_indices], axis = 1) 
+            return times[query][:], np.mean(data[query, i_indices, j_indices], axis=1)
         else:
             query = (times >= startDate) & (times <= endDate)
             for i in xrange(len(times)):
                 if not query[i]: continue
                 if times[i].month not in months:
                     query[i] = False
-            return times[query][:], np.mean(data[query, i_indices, j_indices], axis = 1)
-
+            return times[query][:], np.mean(data[query, i_indices, j_indices], axis=1)
 
 
     def _readFromTxtFile(self, path):
@@ -187,7 +186,7 @@ class SweHolder:
         return data in form of matrix (nx * ny) from path
         """
         data = []
-        f = codecs.open(path,mode="r", encoding="utf8")
+        f = codecs.open(path, mode="r", encoding="utf8")
         lines = f.readlines()
         #lines = map(lambda x: x.rstrip(), lines)
         for line in lines:
@@ -195,14 +194,13 @@ class SweHolder:
             data.extend(map(float, line.split()))
         data = np.array(data)
         data = np.reshape(data, (self.nEastWest, self.nNorthSouth), order="F")
-        data = data[:,::-1]
+        data = data[:, ::-1]
         f.close()
         return data
 
-    def convertAllDataToNetcdf(self, pathToNetCDF = 'data/swe_ross_brown/swe.nc'):
+    def convertAllDataToNetcdf(self, pathToNetCDF='data/swe_ross_brown/swe.nc4'):
 
-        ds = Dataset(pathToNetCDF, mode = 'w', format = 'NETCDF4_CLASSIC')
-        
+        ds = Dataset(pathToNetCDF, mode='w', format='NETCDF4_CLASSIC')
 
         ds.createDimension('time', len(self.all_dates))
         ds.createDimension('lon', self.lons2d.shape[0])
@@ -214,14 +212,12 @@ class SweHolder:
         sweVariable = ds.createVariable('SWE', 'f4', ('time', 'lon', 'lat'))
         sweVariable.units = 'mm of equivalent water'
 
-        timeVariable = ds.createVariable('time', 'i4', ('time'))
+        timeVariable = ds.createVariable('time', 'i4', ('time',))
         ncSinceFormat = '%Y-%m-%d %H:%M:%S'
-        timeVariable.units = ' hours since ' + self.getStartDate().strftime(ncSinceFormat)
+        timeVariable.units = 'hours since ' + self.getStartDate().strftime(ncSinceFormat)
 
-
-        lonVariable[:] = self.lons2d[:, :]
-        latVariable[:] = self.lats2d[:, :]
-
+        lonVariable[:] = self.lons2d
+        latVariable[:] = self.lats2d
 
         nDates = len(self.all_dates)
         for i, theDate in enumerate(self.all_dates):
@@ -230,9 +226,10 @@ class SweHolder:
             sweVariable[i, :, :] = data[:, :]
             print ' {0} / {1} '.format(i, nDates)
 
-        timeVariable[:] = date2num( self.all_dates, units = timeVariable.units)
-        ds.close()
+        timeVariable[:] = date2num(self.all_dates, units=timeVariable.units)
 
+
+        ds.close()
 
 
     def getMeanDataForPoints(self, geopointList=None, startDate=None, endDate=None):
@@ -250,7 +247,6 @@ class SweHolder:
             i_indices.append(i)
             j_indices.append(j)
 
-
         dates = []
         result = {}
         for fileName in os.listdir(self.root_path):
@@ -261,7 +257,6 @@ class SweHolder:
 
             filePath = os.path.join(self.root_path, fileName)
 
-
             data = self._readFromTxtFile(filePath)
 
             dates.append(d)
@@ -269,7 +264,6 @@ class SweHolder:
             result[d] = np.mean(data[i_indices, j_indices])
         dates.sort()
         return dates, [result[d] for d in dates]
-
 
         pass
 
@@ -287,7 +281,6 @@ class SweHolder:
             raise Exception("indices i = {0}; j = {1} are out of the domain bounds".format(i, j))
 
         result = {}
-        
 
         for fileName in os.listdir(self.root_path):
             filePath = os.path.join(self.root_path, fileName)
@@ -298,7 +291,7 @@ class SweHolder:
 
         pass
 
-    def plot_mean(self, year = None, month = None):
+    def plot_mean(self, year=None, month=None):
 
         data = []
         if year is not None:
@@ -313,16 +306,15 @@ class SweHolder:
                 print the_fname[start_index:]
                 x = self._readFromTxtFile(os.path.join(self.root_path, the_fname))
                 data.append(x)
-        the_mean = np.mean(data, axis = 0)
-
+        the_mean = np.mean(data, axis=0)
 
         plt.figure()
-        b = Basemap(projection="cyl", llcrnrlon=self.lons2d[0,0], llcrnrlat=self.lats2d[0,0],
-            urcrnrlon=self.lons2d[-1,-1], urcrnrlat=self.lats2d[-1, -1]
+        b = Basemap(projection="cyl", llcrnrlon=self.lons2d[0, 0], llcrnrlat=self.lats2d[0, 0],
+                    urcrnrlon=self.lons2d[-1, -1], urcrnrlat=self.lats2d[-1, -1]
         )
         x, y = b(self.lons2d, self.lats2d)
-        levels = [10,] + range(20, 120, 20) + [150,200, 300,500,1000]
-        cmap = mpl.cm.get_cmap(name="jet_r", lut = len(levels))
+        levels = [10, ] + range(20, 120, 20) + [150, 200, 300, 500, 1000]
+        cmap = mpl.cm.get_cmap(name="jet_r", lut=len(levels))
         norm = colors.BoundaryNorm(levels, cmap.N)
 
         print np.ma.max(the_mean)
@@ -330,8 +322,8 @@ class SweHolder:
         the_mask = self._get_na_0_25_mask()
         the_mean = np.ma.masked_where(the_mask == 0, the_mean)
 
-        img = b.contourf(x, y, the_mean, levels = levels, cmap = cmap, norm = norm)
-        plt.colorbar(img,ticks = levels, boundaries = levels)
+        img = b.contourf(x, y, the_mean, levels=levels, cmap=cmap, norm=norm)
+        plt.colorbar(img, ticks=levels, boundaries=levels)
         b.drawcoastlines()
         plt.title("mean over {0}-{1}".format(month, year))
         plt.savefig("swe_mean_{0}_{1}.png".format(month, year))
@@ -339,11 +331,11 @@ class SweHolder:
         pass
 
 
-    def plot(self, path = ''):
+    def plot(self, path=''):
         data = self._readFromTxtFile(path)
         plt.figure()
-        b = Basemap(projection="cyl", llcrnrlon=self.lons2d[0,0], llcrnrlat=self.lats2d[0,0],
-            urcrnrlon=self.lons2d[-1,-1], urcrnrlat=self.lats2d[-1, -1]
+        b = Basemap(projection="cyl", llcrnrlon=self.lons2d[0, 0], llcrnrlat=self.lats2d[0, 0],
+                    urcrnrlon=self.lons2d[-1, -1], urcrnrlat=self.lats2d[-1, -1]
         )
         x, y = b(self.lons2d, self.lats2d)
         b.contourf(x, y, data)
@@ -354,10 +346,10 @@ class SweHolder:
 
     pass
 
+
 def test():
     application_properties.set_current_directory()
     swe = SweHolder(path="data/swe_ross_brown/B2003_daily_swe")
-    print netCDF4.__version__
     swe.convertAllDataToNetcdf()
 
 #    swe.plot(path = "data/swe_ross_brown/B2003_daily_swe/1985010112.txt")
@@ -365,8 +357,8 @@ def test():
 #    swe.plot(path = 'data/swe_ross_brown/derksen/1985080112')
 #    plt.show()
 
-    #plt.savefig("swe_1985042312.png")
-    #swe.plot_mean(year=None,month = 3)
+#plt.savefig("swe_1985042312.png")
+#swe.plot_mean(year=None,month = 3)
 
 if __name__ == "__main__":
     application_properties.set_current_directory()

src/diagnose_ccc/plot_projected_changes.py
@@ -1,4 +1,5 @@
 from multiprocessing.pool import Pool
+from netCDF4 import Dataset
 import os
 import pickle
 import re
@@ -446,6 +447,37 @@ def plot_precip(data_path = "/home/huziy/skynet1_rech3/crcm4_data"):
 
     pass
 
+
+
+
+def _generate_mask_of_domain_of_interest( x_indices, y_indices):
+    path = "mask_of_quebec_basins_on_amno_ps_grid.nc"
+    ds = Dataset(path, mode = "w")
+
+
+    the_shape = polar_stereographic.lons.shape
+
+    mask = np.zeros(the_shape, dtype=int)
+    mask[x_indices, y_indices] = 1
+
+
+    ds.createDimension("lon", the_shape[0])
+    ds.createDimension("lat", the_shape[1])
+
+    maskVar = ds.createVariable("mask", "b", dimensions=("lon", "lat"))
+
+
+    lonVar = ds.createVariable("lon", "f4", dimensions=("lon", "lat"))
+    latVar = ds.createVariable("lat", "f4", dimensions=("lon", "lat"))
+
+
+    maskVar[:] = mask[:]
+    lonVar[:] = polar_stereographic.lons
+    latVar[:] = polar_stereographic.lats
+
+    ds.close()
+    pass
+
 def plot_swe():
     seasons = ["(a) Annual", " (b) Winter (DJF)", "(c) Spring (MAM)", "(d) Summer (JJA)", "(e) Fall (SON)"]
     months =  [range(1, 13), [12, 1, 2], [3, 4, 5], [6, 7, 8], [9, 10, 11]  ]
@@ -458,7 +490,8 @@ def plot_swe():
     i_array, j_array = _get_routing_indices()
     x_min, x_max, y_min, y_max = plot_utils.get_ranges(x[i_array, j_array], y[i_array, j_array])
 
-
+    _generate_mask_of_domain_of_interest(i_array, j_array)
+    if True: return
 
     plot_utils.apply_plot_params(width_pt= None, font_size=9, aspect_ratio=2.5)
     fig = plt.figure()

src/narccap/prepare_runoff_for_routing.py
@@ -12,27 +12,37 @@ from netCDF4 import num2date
 
 from plot2D.map_parameters import polar_stereographic
 
-def interpolate_to_amno(data_folder, start_year = 1970, end_year = 1999, rcm = "", gcm = "", out_folder = ""):
-    print "data_folder: {0}".format( data_folder )
+
+def interpolate_to_amno(data_folder, start_year=1970, end_year=1999, rcm="", gcm="", out_folder=""):
+    print "data_folder: {0}".format(data_folder)
+
+    #check if the result file already exists
+    sim_folder = os.path.join(out_folder, "{0}-{1}_{2}-{3}".format(gcm, rcm, start_year, end_year))
+    #create a folder for each simulation
+    if not os.path.isdir(sim_folder):
+        os.mkdir(sim_folder)
+
+    out_path = os.path.join(sim_folder, "narccap_runoff_{0}-{1}_{2}-{3}.nc".format(start_year, end_year, gcm, rcm))
+
+    if os.path.isfile(out_path):
+        print("{0} already exists, remove if you want to recreate.".format(out_path))
+        return
+
     srof_pattern = os.path.join(data_folder, "mrros_*_*_*.nc")
     trof_pattern = os.path.join(data_folder, "mrro_*_*_*.nc")
     srof_ds = MFDataset(srof_pattern)
     trof_ds = MFDataset(trof_pattern)
 
-
     lon_in = srof_ds.variables["lon"][:]
     lat_in = srof_ds.variables["lat"][:]
-
 
     x_in, y_in, z_in = lat_lon.lon_lat_to_cartesian(lon_in.flatten(), lat_in.flatten())
 
     tree = KDTree(zip(x_in, y_in, z_in))
 
-
     lon_out, lat_out = polar_stereographic.lons.flatten(), polar_stereographic.lats.flatten()
     x_out, y_out, z_out = lat_lon.lon_lat_to_cartesian(lon_out, lat_out)
     distances, indices = tree.query(zip(x_out, y_out, z_out))
-
 
     time_var = srof_ds.variables["time"]
     time_in_units = time_var[:]
@@ -40,23 +50,17 @@ def interpolate_to_amno(data_folder, start_year = 1970, end_year = 1999, rcm = "
     times = num2date(time_in_units, time_var.units)
 
     time_indices = np.where(
-      np.array( map(lambda x: start_year <= x.year <= end_year, times), dtype=np.bool )
+        np.array(map(lambda x: start_year <= x.year <= end_year, times), dtype=np.bool)
     )[0]
 
-    srof_sub = srof_ds.variables["mrros"][time_indices,:,:]
-    trof_sub = trof_ds.variables["mrro"][time_indices,:,:]
+    srof_sub = srof_ds.variables["mrros"][time_indices, :, :]
+    trof_sub = trof_ds.variables["mrro"][time_indices, :, :]
 
     times_sub = itertools.ifilter(lambda x: start_year <= x.year <= end_year, times)
     print("selected time window data")
 
 
     #writing result to netcdf
-    sim_folder = os.path.join(out_folder, "{0}-{1}_{2}-{3}".format(gcm, rcm, start_year, end_year))
-    #create a folder for each simulation
-    if not os.path.isdir(sim_folder):
-        os.mkdir(sim_folder)
-
-    out_path = os.path.join(sim_folder, "narccap_runoff_{0}-{1}_{2}-{3}.nc".format(start_year, end_year, gcm, rcm))
     out_nc = Dataset(out_path, "w")
 
     out_nc.createDimension("x", polar_stereographic.lons.shape[0])
@@ -65,7 +69,6 @@ def interpolate_to_amno(data_folder, start_year = 1970, end_year = 1999, rcm = "
 
     srof_var = out_nc.createVariable("mrros", "f4", dimensions=("time", "x", "y"))
     trof_var = out_nc.createVariable("mrro", "f4", dimensions=("time", "x", "y"))
-
 
     assert isinstance(srof_var, Variable)
 
@@ -77,13 +80,11 @@ def interpolate_to_amno(data_folder, start_year = 1970, end_year = 1999, rcm = "
     trof_in_var = trof_ds.variables["mrro"]
     for attr_name in trof_in_var.ncattrs():
         print attr_name
-        trof_var.setncattr(attr_name, getattr( trof_in_var, attr_name))
-
+        trof_var.setncattr(attr_name, getattr(trof_in_var, attr_name))
 
     t_var = out_nc.createVariable("time", "f4", dimensions=("time",))
     lon_var = out_nc.createVariable("longitude", "f4", dimensions=( "x", "y"))
     lat_var = out_nc.createVariable("latitude", "f4", dimensions=("x", "y"))
-
 
     t_var.units = time_var.units
     print("interpolating and saving data to netcdf file")
@@ -91,22 +92,21 @@ def interpolate_to_amno(data_folder, start_year = 1970, end_year = 1999, rcm = "
 
     #interpolate in time if necessary
     n_interps = 0
-    for i, t in enumerate( times_sub ):
-        sr_slice = srof_sub[i,:,:].flatten()
+    for i, t in enumerate(times_sub):
+        sr_slice = srof_sub[i, :, :].flatten()
         tr_slice = trof_sub[i, :, :].flatten()
 
         trof1 = tr_slice[indices].reshape(nrows, ncols)
         srof1 = sr_slice[indices].reshape(nrows, ncols)
 
-
         if hasattr(trof1, "mask") and np.all(trof1.mask):
-            trof1 = trof_var[i-1,:,:]
+            trof1 = trof_var[i - 1, :, :]
             n_interps += 1
         if hasattr(srof1, "mask") and np.all(srof1.mask):
-            srof1 = srof_var[i-1,:,:]
+            srof1 = srof_var[i - 1, :, :]
 
-        trof_var[i,:,:] = trof1
-        srof_var[i,:,:] = srof1
+        trof_var[i, :, :] = trof1
+        srof_var[i, :, :] = srof1
         t_var[i] = date2num(t, time_var.units)
 
     print "Number of interpolations in time: {0}".format(n_interps)
@@ -115,22 +115,20 @@ def interpolate_to_amno(data_folder, start_year = 1970, end_year = 1999, rcm = "
     out_nc.close()
 
 
-
-def main(gcm = "", rcm = "", out_folder = ""):
-
+def main(gcm="", rcm="", out_folder=""):
     pc = None
     pf = None
 
     kwargs = {
         "start_year": 1970,
         "end_year": 1999,
-        "rcm" : rcm, "gcm" : gcm,
-        "out_folder" : out_folder
+        "rcm": rcm, "gcm": gcm,
+        "out_folder": out_folder
     }
     in_folder = "data/narccap/{0}-{1}/current".format(gcm, rcm)
 
     if os.path.isdir(in_folder):
-        pc = Process(target=interpolate_to_amno, args=(in_folder, ), kwargs = kwargs )
+        pc = Process(target=interpolate_to_amno, args=(in_folder, ), kwargs=kwargs)
         pc.start()
     else:
         print "{0} does not exist, ignoring the period ...".format(in_folder)
@@ -138,12 +136,12 @@ def main(gcm = "", rcm = "", out_folder = ""):
     kwargs = {
         "start_year": 2041,
         "end_year": 2070,
-        "rcm" : rcm, "gcm" : gcm,
-        "out_folder" : out_folder
+        "rcm": rcm, "gcm": gcm,
+        "out_folder": out_folder
     }
-    in_folder =  "data/narccap/{0}-{1}/future".format(gcm, rcm)
+    in_folder = "data/narccap/{0}-{1}/future".format(gcm, rcm)
     if os.path.isdir(in_folder):
-        pf = Process(target=interpolate_to_amno, args=(in_folder, ), kwargs = kwargs )
+        pf = Process(target=interpolate_to_amno, args=(in_folder, ), kwargs=kwargs)
         pf.start()
     else:
         print "{0} does not exist, ignoring the period ...".format(in_folder)
@@ -153,23 +151,21 @@ def main(gcm = "", rcm = "", out_folder = ""):
     if pf is not None: pf.join()
 
 
-
-
 #entry
 if __name__ == "__main__":
     import application_properties
+
     application_properties.set_current_directory()
 
     import time
+
     t0 = time.clock()
 
-
-
-    gcm_list = ["ccsm",  "ccsm",  "cgcm3",  "cgcm3",  "cgcm3",  "gfdl",  "gfdl",  "gfdl",  "hadcm3"]
-    rcm_list = ["crcm",  "wrfg",  "crcm",   "rcm3",   "wrfg",  "ecp2",  "hrm3",     "rcm3",  "hrm3"]
+    gcm_list = ["ccsm", "ccsm", "cgcm3", "cgcm3", "cgcm3", "gfdl", "gfdl", "gfdl", "hadcm3", "ccsm"]
+    rcm_list = ["crcm", "wrfg", "crcm", "rcm3", "wrfg", "ecp2", "hrm3", "rcm3", "hrm3", "mm5i"]
 
     for gcm, rcm in zip(gcm_list, rcm_list):
-        main(gcm = gcm, rcm = rcm, out_folder = "/home/huziy/skynet1_rech3/narccap_prepared_runoff_for_routing")
+        main(gcm=gcm, rcm=rcm, out_folder="/home/huziy/skynet1_rech3/narccap_prepared_runoff_for_routing")
     print("Execution time {0} seconds".format(time.clock() - t0))
     print "Hello world"
   